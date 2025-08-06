37513b5d5e32a1f5d679d87b390ce2e824bfccf6
Илья <ilia@MacBook-Pro-Sergey.local>
2025-08-06 13:12:43 -0700

    FINAL

--- String.hpp
+++ String.hpp
@@ -1,221 +1,270 @@
-#ifndef STRING_HPP
-#define STRING_HPP
-
-#include <cstddef>
-#include <stdexcept>
-#include <utility>
+#pragma once
 #include <iostream>
-#include <string>
+#include <stdexcept>
+#include <iterator>
+#include <algorithm>
+#include <utility>
+#include <cstring>
+
+// Пользовательское исключение
+class StringException : public std::runtime_error {
+public:
+    explicit StringException(const std::string& msg) : std::runtime_error(msg) {}
+};
 
 template<typename T>
 class String {
 private:
     T* data_;
-    std::size_t size_;
-    std::size_t capacity_;
+    size_t size_;
+    size_t capacity_;
 
-    void copy_from(const T* src, std::size_t count) {
-        size_ = capacity_ = count;
-        data_ = (count > 0) ? new T[capacity_] : nullptr;
-        for (std::size_t i = 0; i < count; ++i) {
-            data_[i] = src[i];
+    void allocate(size_t new_cap) {
+        T* new_data = new T[new_cap + 1]{};
+        if (data_) {
+            std::copy(data_, data_ + size_, new_data);
+            delete[] data_;
         }
+        data_ = new_data;
+        capacity_ = new_cap;
+        data_[size_] = T{};
+    }
+
+    void copy_from(const String& other) {
+        size_ = other.size_;
+        capacity_ = other.capacity_;
+        data_ = new T[capacity_ + 1];
+        std::copy(other.data_, other.data_ + size_, data_);
+        data_[size_] = T{};
     }
 
 public:
-    String() : data_(nullptr), size_(0), capacity_(0) {}
+    // Конструкторы
+    String() : data_(new T[1]{T{}}), size_(0), capacity_(0) {}
 
-    String(const String& other) { copy_from(other.data_, other.size_); }
-
-    String(String&& other) noexcept : data_(other.data_), size_(other.size_), capacity_(other.capacity_) {
-        other.data_ = nullptr;
-        other.size_ = 0;
-        other.capacity_ = 0;
+    String(size_t n, const T& ch) : size_(n), capacity_(n) {
+        data_ = new T[n + 1];
+        std::fill(data_, data_ + n, ch);
+        data_[n] = T{};
     }
 
-    String(std::size_t count, const T& ch) : size_(count), capacity_(count) {
-        data_ = (count > 0) ? new T[count] : nullptr;
-        for (std::size_t i = 0; i < count; ++i) {
-            data_[i] = ch;
-        }
-    }
-
-    String(const T* cstr) : size_(0), capacity_(0), data_(nullptr) {
-        if (cstr) {
-            while (cstr[size_] != T{}) ++size_;
-            copy_from(cstr, size_);
-        }
+    String(const T* cstr) {
+        if (!cstr)
+            throw StringException("nullptr passed to constructor");
+        size_ = 0;
+        while (cstr[size_] != T{})
+            ++size_;
+        capacity_ = size_;
+        data_ = new T[size_ + 1];
+        std::copy(cstr, cstr + size_, data_);
+        data_[size_] = T{};
     }
 
     String(const T* begin, const T* end) {
-        if (begin > end) throw std::invalid_argument("begin must not be greater than end");
-        size_ = end - begin;
-        copy_from(begin, size_);
+        if (!begin || !end || end < begin)
+            throw std::invalid_argument("Invalid range in constructor");
+        size_ = static_cast<size_t>(end - begin);
+        capacity_ = size_;
+        data_ = new T[size_ + 1];
+        std::copy(begin, end, data_);
+        data_[size_] = T{};
     }
 
     template<typename U>
-    explicit String(const String<U>& other) {
-        size_ = capacity_ = other.size();
-        data_ = (size_ > 0) ? new T[size_] : nullptr;
-        for (std::size_t i = 0; i < size_; ++i) {
-            data_[i] = static_cast<T>(other[i]);
-        }
+    explicit String(const String<U>& other)
+        : size_(other.size()), capacity_(other.size()) {
+        data_ = new T[capacity_ + 1];
+        std::transform(other.begin(), other.end(), data_, [](const U& ch) {
+            return static_cast<T>(ch);
+        });
+        data_[size_] = T{};
+    }
+
+    // Копирование и перемещение
+    String(const String& other) { copy_from(other); }
+
+    String(String&& other) noexcept
+        : data_(other.data_), size_(other.size_), capacity_(other.capacity_) {
+        other.data_ = nullptr;
+        other.size_ = other.capacity_ = 0;
     }
 
     ~String() { delete[] data_; }
 
-    String& operator=(const String& other) {
-        if (this != &other) {
-            delete[] data_;
-            copy_from(other.data_, other.size_);
-        }
+    String& operator=(String other) {
+        swap(*this, other);
         return *this;
     }
 
-    String& operator=(String&& other) noexcept {
-        if (this != &other) {
-            delete[] data_;
-            data_ = other.data_;
-            size_ = other.size_;
-            capacity_ = other.capacity_;
-            other.data_ = nullptr;
-            other.size_ = 0;
-            other.capacity_ = 0;
-        }
-        return *this;
+    friend void swap(String& a, String& b) noexcept {
+        using std::swap;
+        swap(a.data_, b.data_);
+        swap(a.size_, b.size_);
+        swap(a.capacity_, b.capacity_);
     }
 
-    std::size_t size() const { return size_; }
-
+    // Методы
+    size_t size() const { return size_; }
     bool empty() const { return size_ == 0; }
 
     void clear() {
         delete[] data_;
-        data_ = nullptr;
-        size_ = capacity_ = 0;
+        data_ = new T[1]{T{}};
+        size_ = 0;
+        capacity_ = 0;
     }
 
-    T& operator[](std::size_t index) {
-        if (index >= size_) throw std::out_of_range("Index out of bounds");
-        return data_[index];
+    const T& operator[](size_t i) const {
+        if (i >= size_) throw std::out_of_range("Index out of range");
+        return data_[i];
     }
 
-    const T& operator[](std::size_t index) const {
-        if (index >= size_) throw std::out_of_range("Index out of bounds");
-        return data_[index];
+    T& operator[](size_t i) {
+        if (i >= size_) throw std::out_of_range("Index out of range");
+        return data_[i];
     }
 
-    String substr(std::size_t pos, std::size_t len) const {
-        if (pos > size_) throw std::out_of_range("substr: position out of range");
-        if (pos + len > size_) len = size_ - pos;
-        return String(data_ + pos, data_ + pos + len);
-    }
-
-    String operator+(const String& other) const {
-        String result;
-        result.size_ = result.capacity_ = size_ + other.size_;
-        result.data_ = new T[result.size_];
-        for (std::size_t i = 0; i < size_; ++i) result.data_[i] = data_[i];
-        for (std::size_t i = 0; i < other.size_; ++i) result.data_[size_ + i] = other.data_[i];
-        return result;
-    }
-
-    String operator+(const T& ch) const {
-        String result;
-        result.size_ = result.capacity_ = size_ + 1;
-        result.data_ = new T[result.size_];
-        for (std::size_t i = 0; i < size_; ++i) result.data_[i] = data_[i];
-        result.data_[size_] = ch;
-        return result;
-    }
-
-    friend String operator+(const T& ch, const String& str) {
-        String result;
-        result.size_ = result.capacity_ = str.size_ + 1;
-        result.data_ = new T[result.size_];
-        result.data_[0] = ch;
-        for (std::size_t i = 0; i < str.size_; ++i) result.data_[i + 1] = str.data_[i];
-        return result;
+    String substr(size_t pos, size_t len) const {
+        if (pos > size_) throw std::out_of_range("Substring position out of range");
+        size_t actual_len = std::min(len, size_ - pos);
+        return String(data_ + pos, data_ + pos + actual_len);
     }
 
     void push_back(const T& ch) {
         if (size_ == capacity_) {
-            capacity_ = capacity_ * 2 + 1;
-            T* new_data = new T[capacity_];
-            for (std::size_t i = 0; i < size_; ++i) new_data[i] = data_[i];
-            delete[] data_;
-            data_ = new_data;
+            size_t new_cap = (capacity_ == 0) ? 1 : capacity_ * 2;
+            reserve(new_cap);
         }
         data_[size_++] = ch;
+        data_[size_] = T{};
     }
 
-    String operator*(int count) const {
-        if (count <= 0) return String();
-        String result;
-        result.size_ = result.capacity_ = size_ * count;
-        result.data_ = new T[result.size_];
-        for (int i = 0; i < count; ++i)
-            for (std::size_t j = 0; j < size_; ++j)
-                result.data_[i * size_ + j] = data_[j];
-        return result;
+    void reserve(size_t new_cap) {
+        if (new_cap > capacity_)
+            allocate(new_cap);
     }
 
-    friend String operator*(int count, const String& str) {
-        return str * count;
+    // Итераторы
+    T* begin() { return data_; }
+    T* end() { return data_ + size_; }
+    const T* begin() const { return data_; }
+    const T* end() const { return data_ + size_; }
+
+    // Конкатенация
+    friend String operator+(const String& lhs, const String& rhs) {
+        String res(lhs.size_ + rhs.size_, T{});
+        std::copy(lhs.data_, lhs.data_ + lhs.size_, res.data_);
+        std::copy(rhs.data_, rhs.data_ + rhs.size_, res.data_ + lhs.size_);
+        res.data_[res.size_] = T{};
+        return res;
     }
 
-    bool operator==(const String& other) const {
-        if (size_ != other.size_) return false;
-        for (std::size_t i = 0; i < size_; ++i)
-            if (!(data_[i] == other.data_[i])) return false;
-        return true;
+    friend String operator+(const String& lhs, const T& ch) {
+        String res(lhs);
+        res.push_back(ch);
+        return res;
     }
 
-    bool operator!=(const String& other) const {
-        return !(*this == other);
+    friend String operator+(const T& ch, const String& rhs) {
+        String res(1, ch);
+        return res + rhs;
     }
 
-    bool operator<(const String& other) const {
-        std::size_t min_len = (size_ < other.size_) ? size_ : other.size_;
-        for (std::size_t i = 0; i < min_len; ++i) {
-            if (data_[i] < other.data_[i]) return true;
-            if (data_[i] > other.data_[i]) return false;
-        }
-        return size_ < other.size_;
+    // Повторение
+    friend String operator*(const String& str, size_t times) {
+        String res;
+        res.reserve(str.size_ * times);
+        for (size_t i = 0; i < times; ++i)
+            for (size_t j = 0; j < str.size_; ++j)
+                res.push_back(str.data_[j]);
+        return res;
     }
 
-    bool operator<=(const String& other) const { return *this < other || *this == other; }
-    bool operator>(const String& other) const { return !(*this <= other); }
-    bool operator>=(const String& other) const { return !(*this < other); }
+    friend String operator*(size_t times, const String& str) {
+        return str * times;
+    }
 
-    template <typename Trans>
+    // Сравнение
+    friend bool operator==(const String& a, const String& b) {
+        return a.size_ == b.size_ && std::equal(a.data_, a.data_ + a.size_, b.data_);
+    }
+
+    friend bool operator!=(const String& a, const String& b) {
+        return !(a == b);
+    }
+
+    friend bool operator<(const String& a, const String& b) {
+        return std::lexicographical_compare(a.data_, a.data_ + a.size_,
+                                            b.data_, b.data_ + b.size_);
+    }
+
+    friend bool operator<=(const String& a, const String& b) {
+        return !(b < a);
+    }
+
+    friend bool operator>(const String& a, const String& b) {
+        return b < a;
+    }
+
+    friend bool operator>=(const String& a, const String& b) {
+        return !(a < b);
+    }
+
+    // Трансформации (статические)
+    template<typename Trans>
     void transform_inplace(const Trans& t) {
-        for (std::size_t i = 0; i < size_; ++i) {
+        for (size_t i = 0; i < size_; ++i)
             data_[i] = t(data_[i]);
-        }
+        data_[size_] = T{};
     }
 
-    template <typename Trans>
+    template<typename Trans>
     String transform_copy(const Trans& t) const {
-        String result(*this);
-        result.transform_inplace(t);
-        return result;
+        String res(size_, T{});
+        for (size_t i = 0; i < size_; ++i)
+            res.data_[i] = t(data_[i]);
+        return res;
+    }
+
+    // Трансформации (динамические)
+    struct Transformer {
+        virtual T operator()(const T&) const = 0;
+        virtual ~Transformer() = default;
+    };
+
+    void apply(const Transformer& t) {
+        for (size_t i = 0; i < size_; ++i)
+            data_[i] = t(data_[i]);
+        data_[size_] = T{};
     }
 };
 
-// I/O только для String<char>
-std::ostream& operator<<(std::ostream& os, const String<char>& str) {
-    for (std::size_t i = 0; i < str.size(); ++i) {
+// I/O только для char
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const String<T>& str) {
+    for (size_t i = 0; i < str.size(); ++i)
         os << str[i];
-    }
     return os;
 }
 
+template<>
+std::ostream& operator<<(std::ostream& os, const String<char>& str) {
+    return os << str.begin();
+}
+
 std::istream& operator>>(std::istream& is, String<char>& str) {
-    std::string temp;
-    is >> temp;
-    str = String<char>(temp.c_str());
+    str.clear();
+    char ch;
+    while (is.get(ch)) {
+        if (std::isspace(static_cast<unsigned char>(ch))) break;
+        str.push_back(ch);
+    }
     return is;
 }
 
-#endif // STRING_HPP
+std::ostream& operator<<(std::ostream& os, const String<char>& str) {
+    for (size_t i = 0; i < str.size(); ++i)
+        os << str[i];
+    return os;
+}
+
--- main.cpp
+++ main.cpp
@@ -1,74 +1,60 @@
 #include <iostream>
 #include "String.hpp"
 
+struct UpperCaseTransformer : public String<char>::Transformer {
+    char operator()(const char& ch) const override {
+        return std::toupper(static_cast<unsigned char>(ch));
+    }
+};
+
+struct ShiftTransformer : public String<char>::Transformer {
+    char operator()(const char& ch) const override {
+        return ch + 1;
+    }
+};
+
 int main() {
-    using std::cout;
-    using std::cin;
-    using std::endl;
+    try {
+        String<char> s1("hello");
+        String<char> s2(" world");
+        String<char> s3 = s1 + s2;
+        std::cout << "Concat: " << s3 << '\n';
 
-    // Конструкторы
-    String<char> s1("HelloWorld");
-    String<char> s2(5, '*');
-    String<char> s3(s1);
-    String<char> s4(std::move(s2));
-    const char* abc = "ABCDE";
-    String<char> s5(abc + 1, abc + 4); // "BCD"
+        String<char> s4 = s3 * 2;
+        std::cout << "Repeat: " << s4 << '\n';
 
-    cout << "s1: " << s1 << endl;
-    cout << "s3 (copy of s1): " << s3 << endl;
-    cout << "s4 (moved from s2): " << s4 << endl;
-    cout << "s5 (range ABCDE+1 to +4): " << s5 << endl;
+        std::cout << "Substring (0, 5): " << s4.substr(0, 5) << '\n';
 
-    // substr
-    String<char> sub = s1.substr(5, 5); // "World"
-    cout << "substr(5,5): " << sub << endl;
+        UpperCaseTransformer upper;
+        String<char> s5 = s4.transform_copy(upper);
+        std::cout << "Transform (copy to upper): " << s5 << '\n';
 
-    // Конкатенация
-    String<char> a("Hello"), b(" C++");
-    cout << "a + b: " << (a + b) << endl;
-    cout << "a + '!': " << (a + '!') << endl;
-    cout << "'>' + a: " << ('>' + a) << endl;
+        ShiftTransformer shift;
+        s5.apply(shift);
+        std::cout << "Apply (shift): " << s5 << '\n';
 
-    // push_back
-    String<char> p("Test");
-    p.push_back('!');
-    cout << "After push_back: " << p << endl;
+        std::cout << "Enter a word: ";
+        String<char> input;
+        std::cin >> input;
+        std::cout << "You entered: " << input << '\n';
 
-    // Повторение строки
-    String<char> r("ab");
-    cout << "r * 3: " << (r * 3) << endl;
-    cout << "2 * r: " << (2 * r) << endl;
-    cout << "r * 0: size = " << (r * 0).size() << ", empty? " << ((r * 0).empty() ? "yes" : "no") << endl;
-
-    // Сравнение
-    String<char> x("apple"), y("banana");
-    cout << "x == y: " << (x == y ? "true" : "false") << endl;
-    cout << "x != y: " << (x != y ? "true" : "false") << endl;
-    cout << "x < y: "  << (x < y ? "true" : "false") << endl;
-    cout << "x <= y: " << (x <= y ? "true" : "false") << endl;
-    cout << "x > y: "  << (x > y ? "true" : "false") << endl;
-    cout << "x >= y: " << (x >= y ? "true" : "false") << endl;
-
-    // Трансформация (статик)
-    struct ToUpper {
-        char operator()(char ch) const {
-            return (ch >= 'a' && ch <= 'z') ? (ch - 'a' + 'A') : ch;
+        // Проверка на исключение
+        try {
+            String<char> invalid(nullptr); // должно выбросить исключение
+        } catch (const StringException& e) {
+            std::cout << "[Caught StringException]: " << e.what() << '\n';
         }
-    };
 
-    String<char> word("hello");
-    word.transform_inplace(ToUpper{});
-    cout << "After transform_inplace (ToUpper): " << word << endl;
+        // Проверка substr с выходом за предел
+        try {
+            auto bad = s4.substr(999, 10);
+        } catch (const std::out_of_range& e) {
+            std::cout << "[Caught out_of_range]: " << e.what() << '\n';
+        }
 
-    String<char> original("hello");
-    String<char> upper = original.transform_copy(ToUpper{});
-    cout << "transform_copy (ToUpper): " << upper << ", original: " << original << endl;
-
-    // I/O
-    String<char> input;
-    cout << "Enter a word: ";
-    cin >> input;
-    cout << "You entered: " << input << endl;
+    } catch (const std::exception& e) {
+        std::cerr << "Fatal error: " << e.what() << '\n';
+    }
 
     return 0;
 }
